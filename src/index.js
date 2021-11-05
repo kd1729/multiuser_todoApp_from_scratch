f286dbfc14dead8883d08e93ffad3ae4a3a36d79
onlykingKD <kaustubhdwivedi1729@gmail.com>
2021-11-05 13:51:04 +0530

    Fixed Route Path of App.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Login from "./Components/Login";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Welcome} />
+      <Route exact path="/" component={App} />
       <Route path="/Signup" component={Welcome} />
       <Route path="/App" component={App} />
       <Route path="/Login" component={Login} />
