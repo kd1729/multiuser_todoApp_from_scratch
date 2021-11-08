30ffead81e31dc092da2d86c4316d4c44164f30d
onlykingKD <kaustubhdwivedi1729@gmail.com>
2021-11-08 10:24:23 +0530

    Deleted unnecessary files and lines

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './signuplogin.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Welcome from "./Components/Welcome";
 import App from "./App";
-import Login from "./Components/Login";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/Signup" component={Welcome} />
-      <Route path="/App" component={App} />
-      <Route path="/Login" component={Login} />
-    </Switch>
-  </BrowserRouter >,
+  <App / >,
   document.getElementById('root')
 );
 
